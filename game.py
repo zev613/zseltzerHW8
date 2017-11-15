e70a40970796b149e55dacb92fa2bbaf347101e0
zev613 <zev123@gmail.com>
2017-11-14 22:32:29 -0500

    Beginning of code

---
+++ game.py
@@ -0,0 +1,17 @@
+################################
+# game.py
+# Part of homework 8, problem 2
+################################
+
+import slidingpuzzletest
+
+from slidingpuzzle import *
+
+def main():
+    board = SlidingPuzzle(4,4)
+    board.displayPuzzle()
+
+    #board.scramble(2)
+    #board.displayPuzzle()
+
+main()
---
+++ slidingpuzzle.py
@@ -0,0 +1,13 @@
+################################
+# slidingpuzzle.py
+# Part of homework 8, problem 1
+################################
+
+class SlidingPuzzle:
+    def __init__(self, rows, columns):
+        self.rows = rows
+        self.cols = columns
+
+        for row in range(self.rows):
+            for col in range(self.col):
+                
